d5f641f2b6eecd9524f665c8693af1708fa10709
vijayyennam <vjay11yenam@gmail.com>
2019-01-14 04:25:48 +0530

    Updates

---
+++ precache-manifest.71f7779bfa430a81bf37678450b80284.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e65bf1620a2ffc3411a4",
+    "url": "/live-stock-watch/static/js/runtime~main.e65bf162.js"
+  },
+  {
+    "revision": "0750ce8acf1c7b195d3e",
+    "url": "/live-stock-watch/static/js/main.0750ce8a.chunk.js"
+  },
+  {
+    "revision": "3defd95d58b5b88002bb",
+    "url": "/live-stock-watch/static/js/1.3defd95d.chunk.js"
+  },
+  {
+    "revision": "0750ce8acf1c7b195d3e",
+    "url": "/live-stock-watch/static/css/main.24552e1c.chunk.css"
+  },
+  {
+    "revision": "741444b32117e47fa638de46a2dd1896",
+    "url": "/live-stock-watch/index.html"
+  }
+];
